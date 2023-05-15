178e0f25226213ee4308e8581ef5664ce8b71813
Dutra3 <gabrieldutra45@gmail.com>
2023-05-14 22:25:01 -0300

    add complete todo task function

--- todo/src/components/Todo/index.tsx
+++ todo/src/components/Todo/index.tsx
@@ -7,18 +7,24 @@ interface TodoProps {
       category: string,
       isCompleted: boolean,
     };
-  removeTodo: (text: number) => void;  
+  removeTodo: (id: number) => void;
+  completeTodo: (id: number) => void;
 }
 
-const Todo = ({ todo, removeTodo }: TodoProps) => {
+const Todo = ({ todo, removeTodo, completeTodo }: TodoProps) => {
   return (
-    <div className="todo">
+    <div 
+      className="todo" 
+      style={{textDecoration: todo.isCompleted ? "line-through" : ""}}
+    >
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="complete">Completar</button>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          {todo.isCompleted ? "Desmarcar" : "Completar"}
+        </button>
         <button className="remove" onClick={() => removeTodo(todo.id)}>X</button>
       </div>
     </div>
