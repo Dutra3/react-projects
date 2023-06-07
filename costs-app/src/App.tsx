5f23ab6d97c634aaf0e23f246f54c407a706244b
Dutra3 <gabrieldutra45@gmail.com>
2023-06-07 16:10:23 -0300

    create Navbar component

--- costs-app/src/App.tsx
+++ costs-app/src/App.tsx
@@ -1,28 +1,33 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
 import { Container } from './components/layout';
 import { Home } from './components/pages/Home';
 import { Company } from './components/pages/Company';
 import { Contact } from './components/pages/Contact';
 import { NewProject } from './components/pages/NewProject';
+import { Footer } from './components/Footer';
 import './App.css'
 
 const App = () => {
 
 	return (
 		<Router>
-			<ul>
-				<Link to='/'>Home</Link>
-				<Link to='/company'>Empresa</Link>
-				<Link to='/contact'>Contato</Link>
-				<Link to='/newproject'>Novo Projeto</Link>
-			</ul>
+			<Navbar />
 			<Routes>
-					<Route path='/' element={<Container customClass="min-height"> <Home /> </Container>} />
-					<Route path='/company' element={<Container customClass="min-height"> <Company /> </Container>} />
-					<Route path='/contact' element={<Container customClass="min-height">  <Contact /> </Container>} />
-					<Route path='/newproject' element={<Container customClass="min-height"> <NewProject /> </Container>} />
+					<Route path='/' 
+						element={<Container customClass="min-height"> <Home /> </Container>} 
+					/>
+					<Route path='/company' 
+						element={<Container customClass="min-height"> <Company /> </Container>} 
+					/>
+					<Route path='/contact' 
+						element={<Container customClass="min-height">  <Contact /> </Container>} 
+					/>
+					<Route path='/newproject' 
+						element={<Container customClass="min-height"> <NewProject /> </Container>} 
+					/>
 			</Routes>
-			<p>Footer</p>
+			<Footer />
 		</Router>
 	);
 };
---
+++ costs-app/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { Container } from '../layout';
+import styles from './Navbar.module.css';
+import logo from '../../assets/costs_logo.png';
+
+const Navbar = () => {
+	return (
+		<nav className={styles.navbar}>
+			<Container customClass=''>
+				<Link to='/'><img src={logo} alt='Costs' /></Link>
+				<ul className={styles.list}>
+					<li className={styles.item} ><Link to='/'>Home</Link></li>
+					<li className={styles.item} ><Link to='/company'>Empresa</Link></li>
+					<li className={styles.item} ><Link to='/contact'>Contato</Link></li>
+				</ul>
+			</Container>
+		</nav>
+	);
+};
+
+export { Navbar };
