5d7a0d466a498ba42e8898f56ba92c0d2b76c0d4
Dutra3 <gabrieldutra45@gmail.com>
2023-06-09 18:04:33 -0300

    add content to Contact page

--- costs-app/src/components/pages/Contact/index.tsx
+++ costs-app/src/components/pages/Contact/index.tsx
@@ -1,7 +1,15 @@
+import { Container } from '../../Commom/Container';
+import styles from './Contact.module.css';
+
 const Contact = () => {
-  return (
-    <h1>Contact</h1>
-  )
-}
+	return (
+		<Container customClass='column'>
+			<div className={styles.contact_container}>
+				<h1>Contact: dutrasoftware@gmail.com</h1>
+			</div>
+		</Container>
+		
+	);
+};
 
 export { Contact };
