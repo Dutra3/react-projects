2d83e5e2cef16d628f6c759f3fcdb5234d17e407
Dutra3 <gabrieldutra45@gmail.com>
2023-06-09 17:48:30 -0300

    add content to Company page

--- costs-app/src/components/pages/Company/index.tsx
+++ costs-app/src/components/pages/Company/index.tsx
@@ -1,8 +1,19 @@
+import { Container } from "../../Commom/Container"
+import styles from './Company.module.css';
 
 const Company = () => {
-  return (
-    <h1>Company</h1>
-  )
-}
+	return (
+		<Container customClass="column">
+			<div className={styles.company_container}>
+				<div>
+					<h1>Company DutraSoftware Ltda</h1>
+				</div>
+				<div>
+					<p>CNPJ: 32.508.190/0001-49</p>
+				</div>
+			</div>
+		</Container>
+	);
+};
 
 export { Company };
