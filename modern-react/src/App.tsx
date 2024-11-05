e81544a933b6e5428322061d0806ad8e75e52b34
gabriel <gabrieldutra45@gmail.com>
2024-11-04 22:59:28 -0300

    Create button click button

--- modern-react/src/App.tsx
+++ modern-react/src/App.tsx
@@ -1,21 +1,25 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Ja atualizei o meu elemento
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => {
+
+	const handleClick = () => {
+		console.log('Button was clicked!');
+	};
+
+	return (
+	<div className="App">
+		<header className="App-header">
+			<p>Ja atualizei o meu elemento</p>
+			<a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+			>
+				Iniciando novo porjeto
+			</a>
+		</header>
+		<button onClick={handleClick}>Click button</button>
     </div>
-  );
+	);
 }
 
-export default App;
+export { App };
