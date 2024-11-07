a10602d548bccdd958024ad36e41ae9dd5500d82
gabriel <gabrieldutra45@gmail.com>
2024-11-06 22:47:40 -0300

    Add form to create car

---
+++ classified-ads/src/components/add-form.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Button } from "./button";
+
+export const AddForm = () => {
+    return (
+        <form action="">
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="file" name="img" />
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="text" name="title" placeholder="Digite o nome do carro" className="bg-black text-white text-lg w-full outline-none"/>
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="number" name="price_from" placeholder="Digite o preco antigo" className="bg-black text-white text-lg w-full outline-none"/>
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="number" name="price_to" placeholder="Digite o preco novo" className="bg-black text-white text-lg w-full outline-none"/>
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="checkbox" name="negotiable" />
+                Negociavel?
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="text" name="author_name" placeholder="Digite o nome do vendedor" className="bg-black text-white text-lg w-full outline-none"/>
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <input type="email" name="author_email" placeholder="Digite o email do vendedor" className="bg-black text-white text-lg w-full outline-none"/>
+            </label>
+            <label className="block border border-gray-500 p-4 mb-3">
+                <textarea name="description" rows={4} className=" resize-none bg-black text-white text-lg w-full outline-none"></textarea>
+            </label>
+
+            <Button label="Cadastrar" />
+        </form>
+    );
+};
